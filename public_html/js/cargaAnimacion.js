1e07f3836a39bebea69d36ce4f963a809c9cf8c5
KarenSilva <ktrs_9303@hotmail.com>
2016-12-23 17:31:49 -0500

    modificacion

--- public_html/js/cargaAnimacion.js
+++ public_html/js/cargaAnimacion.js
@@ -26,27 +26,30 @@
     });
     $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
-  
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 120) $('.to-top').fadeIn();
+            else $('.to-top').fadeOut();
+        });
+        $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }
-  }); 
-});
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 900, function(){
+
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+          }
+        }); 
+    });
 
 
