e9c9214f9a3d899bc469a79e7315906ac6c69e04
bikerlfh <bikerlfh@hotmail.com>
2016-12-23 18:18:10 -0500

    n/a

--- public_html/controller/enviarContacto.php
+++ public_html/controller/enviarContacto.php
@@ -8,12 +8,12 @@ $email = $conexion->quote($_POST['email']);
 $asunto = $conexion->quote($_POST['asunto']);
 $descripcion = $conexion->quote($_POST['descripcion']);
 
-$query = "INSERT INTO mensaje(nombre,email,asunto,mensaje,fecha) values(".$nombre.",".$email.",".$asunto.",".$descripcion.",'".date('d-m-Y H:m:s')."')";
+$query = "INSERT INTO mensaje(nombre,email,asunto,mensaje,fecha) values(".$nombre.",".$email.",".$asunto.",".$descripcion.",'".date('Y/m/d H:m:s')."')";
 $result = $conexion->query($query);
 if($result == true)
      $result = "Tu mensaje ha sido enviado satisfactoriamente. Pronto te responderemos.";
 else
-    $result = "ERROR: Al intentar guardar el mensaje";
+    $result .= " ERROR: Al intentar guardar el mensaje";
 
 header("Location:../contacto.php?msg=".$result)
 ?>
